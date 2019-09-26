4cd5185fae75916645b72ac5007acf9d8e28fc5e
Nicole Dow <nikki.dow71@gmail.com>
2019-09-26 18:51:26 -0400

    Done.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,7 +2,18 @@ class CommentsController < ApplicationController
 
   def create
     #binding.pry
-    comment = Comment.create(comment_params)
+    # comment = Comment.create(comment_params)
+    if User.find_by_id(comment_params[:user_id])
+      @user = User.find_by_id(comment_params[:user_id])
+      comment = @user.comments.build(comment_params)
+      comment.save
+    elsif comment_params[:user_attributes][:username] != "" && comment_params["user_id"] == ""
+      #make a new user and build a comment of theirs using info in comment_params
+      @user = User.create(comment_params[:user_attributes])
+      comment = @user.comments.build(content: comment_params[:content], post_id: comment_params[:post_id], user_id: @user.id)
+      comment.save
+    end
+
     redirect_to comment.post
   end
 
